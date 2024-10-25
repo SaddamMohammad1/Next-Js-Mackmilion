c905fb33239478ec3bbd1a3369eebb998e844092
saddam <saddammahmod4@gmail.com>
2024-10-25 08:22:11 +0530

    Initial commit

---
+++ foodies-starting-project/05-onwards-foodies-starting-project/lib/meals.js
@@ -0,0 +1,55 @@
+import fs from 'node:fs';
+
+import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
+
+const db = sql('meals.db'); // meals.db is db name
+
+export async function getMeals() {
+    // Here await of 2 sec for add or check loading state
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    // Here added error for checking purpose of error.
+    // throw new Error('Loading meals Failed');
+
+    return db.prepare('SELECT * FROM meals').all();
+}
+
+
+export function getMeal(slug) {
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
+
+export async function saveMeal(meal) {
+    meal.slug = slugify(meal.title, { lower: true });
+    meal.instructions = xss(meal.instructions);
+
+    const extension = meal.image.name.split('.').pop();
+    const fileName = `${meal.slug}.${extension}`;
+
+    const stream = fs.createWriteStream(`public/images/${fileName}`);
+    const bufferedImage = await meal.image.arrayBuffer();
+
+    stream.write(Buffer.from(bufferedImage), (error) => {
+        if (error) {
+            throw new Error('Saving image failed');
+        }
+    });
+
+    meal.image = `/image/${fileName}`;
+
+    db.prepare(`
+        INSERT INTO meals
+            (title, summary, instructions, creator, creator_email, image, slug)
+        VALUES (
+            @title,
+            @summary,
+            @instructions,
+            @creator,
+            @creator_email,
+            @image,
+            @slug
+        )
+    `).run(meal)
+}
